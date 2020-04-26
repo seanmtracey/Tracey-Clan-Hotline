374b4f23b2902c61577335be427536df965e60c0
Sean Tracey <seanmtracey@gmail.com>
2020-04-26 22:29:18 +0100

    Add websockets and beginnings of basic user administration

--- routes/admin.js
+++ routes/admin.js
@@ -2,9 +2,17 @@ const debug = require('debug')('routes:admin');
 const express = require('express');
 const router = express.Router();
 
+const users = require(`${__dirname}/../bin/lib/users`);
+
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+    res.render('admin', { title: 'Manage Users' });
+});
+
+router.post('/add-user', (req, res, next) => {
+
+    
+
 });
 
 module.exports = router;
