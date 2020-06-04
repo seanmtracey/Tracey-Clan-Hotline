da83bef7826aaf61f8c4bc4ee85520e1ebab201e
Sean Tracey <seanmtracey@gmail.com>
2020-06-04 10:22:47 +0100

    Separate account and user paths

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(cookieSession({
 app.use('/', require('./routes/index'));
 app.use('/call', require('./routes/call'));
 app.use('/user', require('./routes/user'));
+app.use('/account', require('./routes/account'));
 app.use('/admin', [admin_auth], require('./routes/admin'));
 
 // catch 404 and forward to error handler
